254e11b93f620490708356ed8a0bb5fb5b246068
Sam Wightwick <info@samwightwick.co.uk>
2021-12-20 17:48:42 +0000

    updating

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # crypto-slack-bot
-Scrapes coinmarketcap and posts to slack daily
-![Image](https://github.com/swightwick/crypto-slack-bot/blob/main/screenshot.png)
 
+Scrapes coinmarketcap using puppeteer, then posts info to a slack channel.
+
+- Add a .env file with your SLACK_TOKEN and CHANNEL_ID to run.
+
+![Image](https://github.com/swightwick/crypto-slack-bot/blob/main/screenshot.png)
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const puppeteer = require('puppeteer');
-const { WebClient } = require('@slack/web-api');
+const {
+  WebClient
+} = require('@slack/web-api');
 // Set Slack Token and Channel ID
 const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
@@ -9,98 +11,92 @@ const web = new WebClient(token);
 const conversationId = process.env.CHANNEL_ID;
 (async () => {
   try {
-		// Scraper
-		const browser = await puppeteer.launch({
-			'args' : [
-				'--no-sandbox',
-				'--disable-setuid-sandbox'
-			]
-		});
-		const page = await browser.newPage()
-		await page.goto('https://coinmarketcap.com/currencies/electroneum/')
+    // Scraper
+    const browser = await puppeteer.launch({
+      'args': [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
+    });
+    const page = await browser.newPage()
+    await page.goto('https://coinmarketcap.com/currencies/electroneum/')
 
     const price = await page.$eval('.priceSection .priceValue span', el => el.innerText)
-		const priceChangePercent = await page.$eval('.priceSection .priceTitle .feeyND', el => el.innerText)
+    const priceChangePercent = await page.$eval('.priceSection .priceTitle .feeyND', el => el.innerText)
     const rank = await page.$eval('.nameSection .namePillPrimary', el => el.innerText)
-		const twentyFourVolume = await page.$eval('.statsBlock:nth-of-type(3) .statsValue', el => el.innerText)
-		const supply = await page.$eval('.n78udj-6.dCjIMS .statsValue', el => el.innerText)
-		console.log(
-			'PRICE ' + price, 
-			'CHANGE ' + priceChangePercent, 
-			'RANK' + rank, 
-			'24hr VOLUME ' + twentyFourVolume, 
-			'SUPPLY ' + supply
-		)
-		await browser.close()
+    const twentyFourVolume = await page.$eval('.statsBlock:nth-of-type(3) .statsValue', el => el.innerText)
+    const supply = await page.$eval('.n78udj-6.dCjIMS .statsValue', el => el.innerText)
+    
+    console.log(
+      'PRICE ' + price,
+      'CHANGE ' + priceChangePercent,
+      'RANK' + rank,
+      '24hr VOLUME ' + twentyFourVolume,
+      'SUPPLY ' + supply
+    )
+    await browser.close()
 
-		// Webhook
-		const res = await web.chat.postMessage(
-		{ 
-			channel: conversationId, 
-			blocks: [
-				{
-					"type": "section",
-					"text": {
-						"type": "mrkdwn",
-						"text": ":zap: *Electroneum Price Update Bot*  :zap:\nCurrent Rank : *" + rank + '*',
-					}
-				},
-				{
-					"type": "divider"
-				},
-				{
-					"type": "section",
-					"fields": [
-						{
-							"type": "mrkdwn",
-							"text": ':moneybag: ETN Price\n*' + price + '*'
-						},
-						{
-							"type": "mrkdwn",
-							"text": ':rocket: Price change\n*' + '*' + priceChangePercent + '*'
-						}
-					]
-				},
-				{
-					"type": "divider"
-				},
-				{
-					"type": "section",
-					"fields": [
-						{
-							"type": "mrkdwn",
-							"text": ':hourglass: 24hr Volume\n*' + twentyFourVolume + '*'
-						},
-						{
-							"type": "mrkdwn",
-							"text": ':hourglass_flowing_sand: Circulating Supply \n*' + supply + '*'
-						}
-					]
-				},
-				{
-					"type": "divider"
-				},
-				{
-					"type": "context",
-					"elements": [
-						{
-							"type": "mrkdwn",
-							"text": "Source <https://coinmarketcap.com/currencies/electroneum/>"
-						}
-					]
-				},
-				{
-					"type": "context",
-					"elements": [
-						{
-							"type": "mrkdwn",
-							"text": "made by github.com/swightwick"
-						}
-					]
-				}]
-		});
+    // Webhook
+    const res = await web.chat.postMessage({
+      channel: conversationId,
+      blocks: [{
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": ":zap: *Electroneum Price Update Bot*  :zap:\nCurrent Rank : *" + rank + '*',
+          }
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "section",
+          "fields": [{
+              "type": "mrkdwn",
+              "text": ':moneybag: ETN Price\n*' + price + '*'
+            },
+            {
+              "type": "mrkdwn",
+              "text": ':rocket: Price change\n*' + priceChangePercent + '*'
+            }
+          ]
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "section",
+          "fields": [{
+              "type": "mrkdwn",
+              "text": ':hourglass: 24hr Volume\n*' + twentyFourVolume + '*'
+            },
+            {
+              "type": "mrkdwn",
+              "text": ':hourglass_flowing_sand: Circulating Supply \n*' + supply + '*'
+            }
+          ]
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "context",
+          "elements": [{
+            "type": "mrkdwn",
+            "text": "Source <https://coinmarketcap.com/currencies/electroneum/>"
+          }]
+        },
+        {
+          "type": "context",
+          "elements": [{
+            "type": "mrkdwn",
+            "text": "made by github.com/swightwick"
+          }]
+        }
+      ]
+    });
     console.log('Message sent: ', res.ts);
-  } catch(e) {
+  } catch (e) {
     console.log('Error from console', e)
   }
 })()
